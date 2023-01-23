3612aa1fa52f34494976bdab77dac4083c6dbe17
Daan Sanders <daansanders@sp-byods-145-109-47-192.wireless.uva.nl>
2023-01-23 12:43:18 +0100

    kleine aanpassingen in functies; refresh, move, print

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -18,18 +18,27 @@ class Grid():
         self.refresh_grid()
 
     def move_car(self, car_type, movement):
-        """Checks if the chosen car is existing. If this is the case checks if the chosen car
-        can move the requested amount of spaces."""
-        for car in self.cars:
-            # Checks if the input car is on the grid
-            if car.type == car_type:
-                x, y = car.col, car.row
-                # Checks if the input car can move the input amount of places
-                if self.can_move(car, movement):
-                    car.move(movement)
-                    self.refresh_grid()
-                    return True
+        """Checks if the chosen car can move the requested amount of spaces."""
+        car = next((car for car in self.cars if car.type == car_type), None)
+        if car and self.can_move(car, movement):
+            car.move(movement)
+            self.refresh_grid()
+            return True
         return False
+
+    # def move_car(self, car_type, movement):
+    #     """Checks if the chosen car is existing. If this is the case checks if the chosen car
+    #     can move the requested amount of spaces."""
+    #     for car in self.cars:
+    #         # Checks if the input car is on the grid
+    #         if car.type == car_type:
+    #             x, y = car.col, car.row
+    #             # Checks if the input car can move the input amount of places
+    #             if self.can_move(car, movement):
+    #                 car.move(movement)
+    #                 self.refresh_grid()
+    #                 return True
+    #     return False
     
     
     def can_move(self, car, movement):
@@ -73,32 +82,51 @@ class Grid():
                     return True
         return False
 
-
     def refresh_grid(self):
         """Goes through the board after a made move and updates/refreshes it."""
         self.board = [['_' for i in range(self.width)] for j in range(self.width)]
-        # Places all the cars in the 2D array
+        # Places the cars in the 2D array
         for car in self.cars:
             x, y = car.col, car.row
-            if car.orientation == "H":
-                for i in range(car.col, car.col + car.length):
-                    self.board[car.row][i] = car.type
-            else:
-                for i in range(car.row, car.row + car.length):
-                    self.board[i][car.col] = car.type 
+            for i in range(car.length):
+                if car.orientation == "H":
+                    self.board[y][x + i] = car.type
+                else:
+                    self.board[y + i][x] = car.type
+
+    # def refresh_grid(self):
+    #     """Goes through the board after a made move and updates/refreshes it."""
+    #     self.board = [['_' for i in range(self.width)] for j in range(self.width)]
+    #     # Places all the cars in the 2D array
+    #     for car in self.cars:
+    #         x, y = car.col, car.row
+    #         if car.orientation == "H":
+    #             for i in range(car.col, car.col + car.length):
+    #                 self.board[car.row][i] = car.type
+    #         else:
+    #             for i in range(car.row, car.row + car.length):
+    #                 self.board[i][car.col] = car.type 
 
     def print_grid(self):
         """Prints the grid with the cars and empty spaces on it."""
-        for i in range(self.width):
-            for j in range(self.width):
-                # Gets the color of the car
-                color = COLORS[self.board[i][j]]
-                # Prints the car or empty place in the right place
-                if len(self.board[i][j]) == 1:
-                    print(f"   {color}{self.board[i][j]}{Style.RESET_ALL}", end="")
-                else:
-                    print(f"  {color}{self.board[i][j]}{Style.RESET_ALL}", end="")
+        for row in self.board:
+            for item in row:
+                color = COLORS[item]
+                print(f"  {color}{item}{Style.RESET_ALL}", end="")
             print()
+
+    # def print_grid(self):
+    #     """Prints the grid with the cars and empty spaces on it."""
+    #     for i in range(self.width):
+    #         for j in range(self.width):
+    #             # Gets the color of the car
+    #             color = COLORS[self.board[i][j]]
+    #             # Prints the car or empty place in the right place
+    #             if len(self.board[i][j]) == 1:
+    #                 print(f"   {color}{self.board[i][j]}{Style.RESET_ALL}", end="")
+    #             else:
+    #                 print(f"  {color}{self.board[i][j]}{Style.RESET_ALL}", end="")
+    #         print()
 
     def __str__(self):
         """(insert description)"""
