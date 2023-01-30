e5287b4877fbc20d3c330f8d53bd4b34b2b099b1
Sarah Blok <nathanchress@hotmail.com>
2023-01-30 16:29:03 +0100

    regex in random experiment

--- experiments/random_experiment.py
+++ experiments/random_experiment.py
@@ -1,27 +1,29 @@
 import subprocess
 import time
 import csv
-
+import re
 
 def timer(duration, run_time):
-        """Creates an experiment file which writes down the number of occurences and moves for the random algorithm"""
-        start = time.time()
-        n_runs = 0
-        experiment_list = []
+    """Creates an experiment file which writes down the number of occurences and moves for the random algorithm"""
+    start = time.time()
+    n_runs = 0
+    experiment_list = []
 
-        while time.time() - start < duration:
-            print(f"run: {n_runs}")
-            output = subprocess.check_output(["timeout", f"{run_time}", "python3", "main.py", "Rushhour6x6_1.csv", "random", "--silent"])
-            test_output = str(output).split(":")
+    while time.time() - start < duration:
+        print(f"run: {n_runs}")
+        output = subprocess.check_output(["timeout", f"{run_time}", "python3", "main.py", "Rushhour6x6_1.csv", "random", "--silent"])
+        output_list = str(output).split("\n")
 
-            print("output = ", test_output)
-                        
-            n_runs += 1
-            # number_moves = 
-            # experiment_list.append(n_runs, number_moves)
+        # Regular expression to find the number after [Amount of Moves:]
+        moves = re.findall(r"(?:\s\d+)", output_list[0])[1]
+        moves = moves.strip()
 
-        with open('results/experiment.csv', "w", newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["iteration", "moves"])
-            for iteration in experiment_list:
-                writer.writerow(iteration)
+        # Increment runs by 1 and add number of moves to experiment file
+        n_runs += 1
+        experiment_list.append((n_runs, moves))
+
+    with open('results/experiment.csv', "w", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["iteration", "moves"])
+        for iteration in experiment_list:
+            writer.writerow(iteration)
