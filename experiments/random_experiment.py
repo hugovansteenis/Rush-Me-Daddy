d563f268955076ad300cfd5b8646a009ba2c3804
Sarah Blok <nathanchress@hotmail.com>
2023-01-30 14:38:02 +0100

    update random experiment

--- experiments/random_experiment.py
+++ experiments/random_experiment.py
@@ -1,22 +1,22 @@
 import subprocess
 import time
+import csv
 
 
 def timer(duration, run_time):
-        """Creates an experiment file which writes down the results from all experiments for the random algorithm"""
+        """Creates an experiment file which writes down the number of occurences and moves for the random algorithm"""
         start = time.time()
         n_runs = 0
         experiment_list = []
 
         while time.time() - start < duration:
             print(f"run: {n_runs}")
-            subprocess.call(["timeout", run_time, "python3", "random"])
+            subprocess.call(["timeout", run_time, "python3", "main.py Rushhour6x6_1.csv random"])
             n_runs += 1
-            stop = time.time()
-            experiment_list.append(n_runs, runnnnntime)
+            experiment_list.append(n_runs, number_moves)
 
         with open('results/experiment.csv', "w", newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["iteration", "time"])
+            writer.writerow(["iteration", "moves"])
             for iteration in experiment_list:
                 writer.writerow(iteration)
